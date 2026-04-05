be47b8c7e51971dcea846827ff99c80b4fe9e112
rohitmalviya05 <malviyarohit180@gmail.com>
2026-04-05 23:57:02 +0530

    changing the way of source code giving to the interpreter

--- src/com/Main.java
+++ src/com/Main.java
@@ -1,11 +1,21 @@
 package com;
+
 import com.interpreter.Interpreter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        String source = """
-            >> x
-        """;
-        Interpreter interpreter = new Interpreter();
-        interpreter.run(source);
+        try {
+            // Path to your file
+            String source = Files.readString(Path.of("Source.txt"));
+
+            Interpreter interpreter = new Interpreter();
+            interpreter.run(source);
+
+        } catch (IOException e) {
+            System.out.println("Error reading file: " + e.getMessage());
+        }
     }
 }
